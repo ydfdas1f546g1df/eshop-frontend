26c9d242636445d40aa1bd65c7fe200c57ea120c
Niklaus Kuelling <niklauskuelling@gmx.ch>
2024-01-01 22:15:42 +0100

    Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,8 @@
     } from '@smui/drawer';
     import List, {Item, Text, Graphic, Separator, Subheader} from '@smui/list';
     import "./+layout.less";
-    import "../theme/dark/_smui-theme.scss";
+    import "../theme/smui.scss";
+    import Button from "@smui/button";
 
     let topAppBar: TopAppBar;
     let darkTheme: boolean | undefined = undefined;
@@ -31,9 +32,8 @@
 
     onMount(() => {
         url = location.pathname;
-        active = (url === '/') ? 'homepage': url.split('/')[1];
+        active = (url === '/') ? 'homepage' : url.split('/')[1];
         console.log(active);
-        darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
         usercookie = document.cookie.split(';').find(row => row.startsWith('user='));
     });
 
@@ -43,97 +43,99 @@
     }
 
 
-
 </script>
 
-<TopAppBar bind:this={topAppBar} variant="standard">
-    <Row>
-        <Section>
-            <IconButton on:click={() => (menuOpen = !menuOpen)}>
-                <Icon tag="svg" viewBox="0 0 24 24">
-                    <path fill="currentColor" d={mdiMenu}/>
-                </Icon>
-            </IconButton>
-            <Title on:click={() => window.location="/"}>Shop</Title>
-        </Section>
-        <Section toolbar id="end">
-            <!-- Make a login button -->
-            {#if usercookie}
-                <IconButton on:click={() => window.location="/logout"} id="login">
-                    <p>Logout</p>
-                </IconButton>
-            {:else}
-                <IconButton on:click={() => window.location="/login"} id="logout">
-                    <p>Login</p>
-                </IconButton>
-            {/if}
-        </Section>
-    </Row>
-</TopAppBar>
-<div id="DrawerCloser" use:clickOutside on:click_outside={() => (menuOpen = false)}>
-    <Drawer variant="modal"  bind:this={drawer}
-            bind:open={menuOpen}>
-        <Header>
-            <DrawerTitle>Shop</DrawerTitle>
-            <Subtitle>It's the best fake shop app drawer.</Subtitle>
-        </Header>
-        <Content>
-            <List>
-                <Item
-                        href="/"
-                        on:click={() => setActive('Homepage')}
-                        activated={active === 'Homepage'}
-                >
-                    <Graphic class="material-icons" aria-hidden="true">home</Graphic>
-                    <Text>Homepage</Text>
-                </Item>
-                <Item
-                        href="/categories"
-                        on:click={() => setActive('categories')}
-                        activated={active === 'categories'}
-                >
-                    <Graphic class="material-icons" aria-hidden="true">explore</Graphic>
-                    <Text>Categories</Text>
-                </Item>
-                <Item
-                        href="shopping_cart"
-                        on:click={() => setActive('shopping_cart')}
-                        activated={active === 'shopping_cart'}
-                >
-                    <Graphic class="material-icons" aria-hidden="true">shopping_cart</Graphic>
-                    <Text>Shopping Cart</Text>
-                </Item>
-                <Separator/>
-                <Subheader tag="h6">Labels</Subheader>
-                <Item
-                        href="login"
-                        on:click={() => setActive('login')}
-                        activated={active === 'login'}
-                >
-                    <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
-                    <Text>Family</Text>
-                </Item>
-                <Item
-                        href="javascript:void(0)"
-                        on:click={() => setActive('Friends')}
-                        activated={active === 'Friends'}
-                >
-                    <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
-                    <Text>Friends</Text>
-                </Item>
-                <Item
-                        href="javascript:void(0)"
-                        on:click={() => setActive('Work')}
-                        activated={active === 'Work'}
-                >
-                    <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
-                    <Text>Work</Text>
-                </Item>
-            </List>
-        </Content>
-    </Drawer>
-</div>
+<style>
+</style>
 
-<AutoAdjust {topAppBar}>
-    <slot/>
-</AutoAdjust>
+    <TopAppBar bind:this={topAppBar} variant="standard">
+        <Row>
+            <Section>
+                <IconButton on:click={() => (menuOpen = !menuOpen)}>
+                    <Icon tag="svg" viewBox="0 0 24 24">
+                        <path fill="currentColor" d={mdiMenu}/>
+                    </Icon>
+                </IconButton>
+                <Title on:click={() => {window.location.assign("/")}}>Shop</Title>
+            </Section>
+            <Section toolbar align="end">
+                <!-- Make a login button -->
+                {#if usercookie}
+                    <Button on:click={() => {window.location.assign("/logout")}} id="logout">
+                        <span>Logout</span>
+                    </Button>
+                {:else}
+                    <Button on:click={() => {window.location.assign("/login")}} id="login">
+                        <span>Login</span>
+                    </Button>
+                {/if}
+            </Section>
+        </Row>
+    </TopAppBar>
+    <div id="DrawerCloser" use:clickOutside on:click_outside={() => (menuOpen = false)}>
+        <Drawer variant="modal" bind:this={drawer}
+                bind:open={menuOpen}>
+            <Header>
+                <DrawerTitle>Shop</DrawerTitle>
+                <Subtitle>It's the best fake shop app drawer.</Subtitle>
+            </Header>
+            <Content>
+                <List>
+                    <Item
+                            href="/"
+                            on:click={() => setActive('Homepage')}
+                            activated={active === 'Homepage'}
+                    >
+                        <Graphic class="material-icons" aria-hidden="true">home</Graphic>
+                        <Text>Homepage</Text>
+                    </Item>
+                    <Item
+                            href="/categories"
+                            on:click={() => setActive('categories')}
+                            activated={active === 'categories'}
+                    >
+                        <Graphic class="material-icons" aria-hidden="true">explore</Graphic>
+                        <Text>Categories</Text>
+                    </Item>
+                    <Item
+                            href="shopping_cart"
+                            on:click={() => setActive('shopping_cart')}
+                            activated={active === 'shopping_cart'}
+                    >
+                        <Graphic class="material-icons" aria-hidden="true">shopping_cart</Graphic>
+                        <Text>Shopping Cart</Text>
+                    </Item>
+                    <Separator/>
+                    <Subheader tag="h6">Labels</Subheader>
+                    <Item
+                            href="login"
+                            on:click={() => setActive('login')}
+                            activated={active === 'login'}
+                    >
+                        <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+                        <Text>Family</Text>
+                    </Item>
+                    <Item
+                            href="javascript:void(0)"
+                            on:click={() => setActive('Friends')}
+                            activated={active === 'Friends'}
+                    >
+                        <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+                        <Text>Friends</Text>
+                    </Item>
+                    <Item
+                            href="javascript:void(0)"
+                            on:click={() => setActive('Work')}
+                            activated={active === 'Work'}
+                    >
+                        <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+                        <Text>Work</Text>
+                    </Item>
+                </List>
+            </Content>
+        </Drawer>
+    </div>
+
+    <AutoAdjust {topAppBar}>
+        <slot/>
+    </AutoAdjust>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,7 +2,6 @@
     import {onMount} from 'svelte';
 
 
-    let items = [];
 
 
 
@@ -11,14 +10,3 @@
     });
 </script>
 
-{#each items as item}
-    <div class-="item" on:click={
-    () => {
-        window.location.href = `/item/${item.id}`;
-    }
-    }>
-        <img src="{item.thumbnailUrl}" alt="Picture of {item.name}">
-        <h3>{item.name}</h3>
-        <p>{item.category}</p>
-    </div>
-{/each}
